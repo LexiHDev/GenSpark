27ffd2921f5872686a08d6a4bcedabb40bd60ed6
Lexi Henson <LexiHensonDev@gmail.com>
2022-06-23 10:54:38 -0500

    move comment to proper place

--- Humans VS Goblins/src/com/genspark/entities/Human.java
+++ Humans VS Goblins/src/com/genspark/entities/Human.java
@@ -7,7 +7,6 @@ public class Human extends Humanoid
 {
 	int finalDmg = 0;
 	int finalHealth = 0;
-	// Names Generated with GPT-3
 	
 	
 	@Override
@@ -23,6 +22,7 @@ public class Human extends Humanoid
 		this.health = 5;
 		updateFinals();
 		List<String> humanNames = Arrays.asList("Aaliyah", "Abigail", "Ada", "Adeline", "Agatha", "Aiden", "Alan", "Alana", "Alec", "Alexandra", "Alfred", "Alice", "Alina", "Alison", "Amos", "Amy", "Ana", "Andre", "Andrew", "Angela", "Ann", "Anna", "Anne", "Annette", "Anthony", "April", "Arthur", "Astrid", "Audrey", "August", "Aurora", "Austin", "Barbara", "Benjamin", "Beth", "Bradley", "Brandon", "Brenda", "Brianna", "Brooke", "Cameron", "Carl", "Carmen", "Carol", "Caroline", "Cassandra", "Cecilia", "Charles", "Charlotte", "Chastity", "Chelsea", "Chester", "Chloe", "Christina", "Christine", "Christopher", "Clara", "Clarence", "Claudia", "Clement", "Clifford", "Connie", "Courtney", "Craig", "Curtis", "Cynthia", "Daisy", "Dale", "Damien", "Dana", "Daniel", "Danielle", "Daphne", "David", "Dawn", "Dean", "Deborah", "Denise", "Dennis", "Diana", "Diego", "Dillon", "Dominic", "Donna", "Doris", "Dorothy", "Edgar", "Eleanor", "Elijah", "Elizabeth", "Ellen", "Emily", "Emma", "Eric", "Evan", "Evelyn", "Faith", "Felix", "Fiona", "Florence", "Frances", "Francis", "Fred", "Gabriel", "Gary", "Gemma", "Genevieve", "Geoffrey", "George", "Geraldine", "Gina", "Glenn", "Grace", "Gregory", "Greta", "Harold", "Harper", "Hayden", "Heather", "Helen", "Henry", "Howard", "Ian", "Ingrid", "Irene", "Lexi");
+		// Names Generated with GPT-3
 		this.name = humanNames.get((int) (Math.random() * humanNames.size()));
 	}
 	
